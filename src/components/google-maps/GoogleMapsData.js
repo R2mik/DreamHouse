2925af3383e2bab7a6e15a76f1da2c3065cefb53
R2dmk <adamik.artur93@gmail.com>
2021-04-20 22:24:21 +0200

    change map comp

--- src/components/google-maps/GoogleMapsData.js
+++ src/components/google-maps/GoogleMapsData.js
@@ -1,6 +1,7 @@
-export const TOWNS = [
+export const SPOTS = [
     {
         id: 1,
+        town: "Białystok",
         street: 'Lipowa 1',
         post: '15-111',
         tel: 'tel. 111-111-111',
@@ -9,6 +10,7 @@ export const TOWNS = [
     },    
     {
         id: 2,
+        town: "Warszawa",
         street: 'Królewska 2',
         post: '00-222',
         tel: 'tel. 222-222-222',
@@ -17,6 +19,7 @@ export const TOWNS = [
     },    
     {
         id: 3,
+        town: "Gdańsk",
         street: 'Angielska 3',
         post: '80-333',
         tel: 'tel. 333-333-333',
--- src/components/google-maps/GoogleMapsView.js
+++ src/components/google-maps/GoogleMapsView.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
 import { GoogleMap, useLoadScript} from "@react-google-maps/api";
+import { SPOTS } from "./GoogleMapsData";
 
-import { TOWNS } from './GoogleMapsData'
+const spotsList = SPOTS.map((lat, lng) => ({...{lat, lng} }))
 
 const libraries = ["places"];
+
 
 const mapContainerStyle = {
         width: "50vw",
         height: "50vh",
 };
+
 const center = {
-        lat:53.133511,
-        lng:23.151649,
+        lat: spotsList.lat,
+        lng: spotsList.lng,
 }
 
 export function GooglemapsView() {
@@ -20,18 +23,24 @@ export function GooglemapsView() {
                 googleMapsApiKey: "AIzaSyAUWeGjaHllqysVRdbjkF3GnLBswlA5TwY",
                 libraries,
         })
-
+        
         if (loadError) return "Error loading maps";
-
+        
         if(!isLoaded) return "Loading Maps";
-
+        
         return(
-                <div>
+                <div className="adress">
                         <GoogleMap 
                                 mapContainerStyle={mapContainerStyle} 
                                 zoom={15}
                                 center={center}                 
                         ></GoogleMap>
+{/*                         <h3>{SPOTS.forEach((spot) => spot.town)}</h3>
+                        <ul className="list-unstyled">
+                                <li className="adr-li">{SPOTS.forEach((spot) => spot.street)}</li>
+                                <li className="adr-li">{SPOTS.forEach((spot) => spot.town + spot.post)}</li>
+                                <li className="adr-li">{SPOTS.forEach((spot) => spot.tel)}</li> 
+                        </ul>*/}
                 </div>
         )
 }
