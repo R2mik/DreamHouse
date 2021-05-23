8e24ec1d9506501afd10764c65167204ad8620c0
R2dmk <adamik.artur93@gmail.com>
2021-05-23 17:00:25 +0200

    fixed all problems with nav in mobile verstion

--- src/components/nav/NavView.js
+++ src/components/nav/NavView.js
@@ -13,13 +13,13 @@ export const NavView = (closeMobileMenu, showSidebar, sidebar) => (
         </header>
         <nav className={sidebar ? 'navbar' : "change navbar"}>
             {/* {...{  onClick: showSidebar, className: ....}} */}
-            <div onClick={()=>showSidebar} className="hamburger-menu">
+            <div onClick={()=>showSidebar()} className="hamburger-menu">
                 <div className='line line-1'></div>
                 <div className='line line-2'></div>
                 <div className='line line-3'></div>
             </div>
             {/* {...{  onClick: showSidebar, className: ....}} */}
-            <ul onClick={()=>showSidebar} className={changeNavBarClassName(sidebar,"navbar", 'change navbar')}>
+            <ul onClick={()=>showSidebar()} className={changeNavBarClassName(sidebar, 'change navbar', "navbar")}>
                 {/* 1. create array with object, 2. iterate this array using method map  */}
                 <Link to="/Projects"  className="td">
                     <li><p className="link">Projects</p></li>
