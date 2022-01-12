92ad9fc3bd47c3f906ac3c6ee400c4aadda253da
R2dmk <adamik.artur93@gmail.com>
2022-01-12 21:53:32 +0100

    correct css

--- src/components/home/HomeView.js
+++ src/components/home/HomeView.js
@@ -5,16 +5,18 @@ import { INCREASE_LEVEL_LIFE } from '../../utils/constants'
 
 export const HomeView = () => (
     <div className='bg'>
-    <video className="bgVideo" autoPlay muted loop>
-        <source src ={require( "../../assets/video/HomeVid.mp4")} type="video/mp4"/>
-    </video>
-    <div className='outer'>
-        <div className='middle'>
-            <div className='inner'>
-                <h1 className="headline">{INCREASE_LEVEL_LIFE}</h1>
-                {<Button className='btns' buttonStyle='btn-outline' buttonSize='btn-large'><b>GET STARTED</b></Button>}
-            </div>
+        <div className="video-cont">
+            <video className="bgVideo" autoPlay muted loop>
+                <source src ={require( "../../assets/video/HomeVid.mp4")} type="video/mp4"/>
+            </video>
         </div>
-    </div>           
-</div>
+        <div className='outer'>
+            <div className='middle'>
+                <div className='inner'>
+                    <h1 className="headline">{INCREASE_LEVEL_LIFE}</h1>
+                    {<Button className='btns' buttonStyle='btn-outline' buttonSize='btn-large'><b>GET STARTED</b></Button>}
+                </div>
+            </div>
+        </div>           
+    </div>
 )
